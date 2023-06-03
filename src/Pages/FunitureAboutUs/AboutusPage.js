01f1e810e43f857c3a4abcf603379fce0fd88052
Ibezimakor <franklinibezimakor@gmail.com>
2023-06-03 15:22:41 +0100

    about page added

--- src/Pages/FunitureAboutUs/AboutusPage.js
+++ src/Pages/FunitureAboutUs/AboutusPage.js
@@ -1,11 +1,20 @@
 import React from "react";
 import Navbar from "../../Componet/Header/Navbar/Navbar";
 import FooterBar from "../../Componet/Footer/FooterBar";
-
+import "./funitureAboutUs.css";
 export default function AboutUsPage() {
   return (
     <div>
       <Navbar />
+      <div>
+        <div className="Production-Service">
+          <h1>Services</h1>
+          <h3>
+            The product crafted by talented crafter and using high quality
+            material with love inside
+          </h3>
+        </div>
+      </div>
       <FooterBar />
     </div>
   );
--- src/Pages/FunitureArticles/ArticlesPage.js
+++ src/Pages/FunitureArticles/ArticlesPage.js
@@ -6,6 +6,7 @@ export default function ArticlesPage() {
   return (
     <div>
       <Navbar />
+      <div></div>
       <FooterBar />
     </div>
   );
